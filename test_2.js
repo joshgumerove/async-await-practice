75247adab5fbbdeb8632d77c88c5f25aa857cc5e
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-02-07 16:04:58 -0500

    make code unblocking by taking it off the main thread

--- test_2.js
+++ test_2.js
@@ -2,14 +2,20 @@ let begin = Date.now();
 
 const output = (x) => console.log(`${x} \n elapsed: ${Date.now() - begin}`);
 
-const blocking = () => {
-  let i = 0;
-  while (i < 100000000) {
-    i++;
-  }
-  return "all done";
+const unblocking = () => {
+  return new Promise((resolve, reject) => {
+    resolve("this now should be last");
+  })
+    .then((resp) => {
+      let i = 0;
+      while (i < 100000000) {
+        i++;
+      }
+      return "this will be logged at end";
+    })
+    .then((data) => console.log(data));
 };
 
 output("syncrhonous 1");
-output(blocking()); // note how passed in function
+output(unblocking()); // note how passed in function
 output("synchrnous 2");
